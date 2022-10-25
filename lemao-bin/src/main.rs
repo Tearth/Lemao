b09bb1375fd33e119b0cf1a4023709bf19b8a5f4
Tearth <tearthdev@gmail.com>
2022-10-25 20:31:33 +0200

    Add WinAPI binding

--- lemao-bin/src/main.rs
+++ lemao-bin/src/main.rs
@@ -1,3 +1,18 @@
+use lemao_winapi::bindings::winapi;
+use std::ffi::CString;
+
 fn main() {
+    let lp_text = CString::new("Hello, world 123!").unwrap();
+    let lp_caption = CString::new("MessageBox Example").unwrap();
+
+    unsafe {
+        winapi::MessageBoxA(
+            std::ptr::null_mut(),
+            lp_text.as_ptr(),
+            lp_caption.as_ptr(),
+            winapi::MB_OK | winapi::MB_ICONINFORMATION,
+        );
+    }
+
     println!("Hello, world!");
 }
---
+++ lemao-winapi/build.rs
@@ -0,0 +1,24 @@
+use std::path::Path;
+
+fn main() {
+    let winapi_binging_path = "./src/bindings/winapi.rs";
+    if !Path::new(winapi_binging_path).exists() {
+        let config = "#define WIN32_LEAN_AND_MEAN";
+        lemao_bindgen::Builder::default()
+            .header(
+                "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.19041.0\\um\\Windows.h",
+            )
+            .header_contents("config.h", config)
+            .parse_callbacks(Box::new(lemao_bindgen::CargoCallbacks))
+            .layout_tests(false)
+            .blocklist_type("_IMAGE_TLS_DIRECTORY64")
+            .blocklist_type("IMAGE_TLS_DIRECTORY64")
+            .blocklist_type("PIMAGE_TLS_DIRECTORY64")
+            .blocklist_type("IMAGE_TLS_DIRECTORY")
+            .blocklist_type("PIMAGE_TLS_DIRECTORY")
+            .generate()
+            .unwrap()
+            .write_to_file(winapi_binging_path)
+            .unwrap();
+    }
+}
