0c5b601672ae6104b76461d1390a76988a165feb
Tearth <tearthdev@gmail.com>
2022-11-06 14:11:42 +0100

    Add OpenAL binding

---
+++ lemao-openal/build.rs
@@ -0,0 +1,17 @@
+use std::path::Path;
+
+fn main() {
+    let openal_binging_path = "./src/bindings/openal.rs";
+    if !Path::new(openal_binging_path).exists() {
+        lemao_bindgen::Builder::default()
+            .header("./src/headers/al.h")
+            .header("./src/headers/alc.h")
+            .clang_args(&["-I./src/headers/"])
+            .parse_callbacks(Box::new(lemao_bindgen::CargoCallbacks))
+            .layout_tests(false)
+            .generate()
+            .unwrap()
+            .write_to_file(openal_binging_path)
+            .unwrap();
+    }
+}
