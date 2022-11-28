37aa49314f51ce96fa91db0f2cd3ea628e382d74
Tearth <tearthdev@gmail.com>
2022-11-29 00:01:27 +0100

    Add initial GLX binding

--- lemao-opengl/build.rs
+++ lemao-opengl/build.rs
@@ -1,6 +1,16 @@
 use std::path::Path;
 
 fn main() {
+    build_core_binding();
+
+    #[cfg(windows)]
+    build_windows_binding();
+
+    #[cfg(unix)]
+    build_linux_binding();
+}
+
+fn build_core_binding() {
     let opengl_binging_path = "./src/bindings/opengl.rs";
     if !Path::new(opengl_binging_path).exists() {
         lemao_bindgen::Builder::default()
@@ -15,7 +25,10 @@ fn main() {
             .write_to_file(opengl_binging_path)
             .unwrap();
     }
+}
 
+#[cfg(windows)]
+fn build_windows_binding() {
     let wgl_binging_path = "./src/bindings/wgl.rs";
     if !Path::new(wgl_binging_path).exists() {
         lemao_bindgen::Builder::default()
@@ -28,6 +41,22 @@ fn main() {
             .generate()
             .unwrap()
             .write_to_file(wgl_binging_path)
+            .unwrap();
+    }
+}
+
+#[cfg(unix)]
+fn build_linux_binding() {
+    let glx_binging_path = "./src/bindings/glx.rs";
+    if !Path::new(glx_binging_path).exists() {
+        lemao_bindgen::Builder::default()
+            .header("/usr/include/GL/glx.h")
+            .parse_callbacks(Box::new(lemao_bindgen::CargoCallbacks))
+            .layout_tests(false)
+            .allowlist_file("/usr/include/GL/glx.h")
+            .generate()
+            .unwrap()
+            .write_to_file(glx_binging_path)
             .unwrap();
     }
 }
--- lemao-opengl/src/bindings/mod.rs
+++ lemao-opengl/src/bindings/mod.rs
@@ -1,3 +1,8 @@
 #![allow(warnings, clippy::all)]
 pub mod opengl;
+
+#[cfg(windows)]
 pub mod wgl;
+
+#[cfg(unix)]
+pub mod glx;
