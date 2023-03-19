6b5431513a2b3c96f340e5c86cc950e340ada7e6
Tearth <tearthdev@gmail.com>
2023-03-19 13:51:44 +0100

    Add fast move mode

--- games/snake/src/systems/sync.rs
+++ games/snake/src/systems/sync.rs
@@ -4,7 +4,7 @@ use super::head::HeadSystem;
 use crate::messages::Message;
 use crate::scenes::game::GameScene;
 use crate::state::global::GlobalAppData;
-use lemao_core::lemao_common_platform::input::InputEvent;
+use lemao_core::lemao_common_platform::input::{InputEvent, Key};
 use lemao_framework::app::Application;
 use lemao_framework::ecs::systems::System;
 use lemao_framework::ecs::world::World;
@@ -16,12 +16,17 @@ pub struct SyncSystem {}
 impl System<GlobalAppData, GameScene, Message> for SyncSystem {
     fn update(
         &mut self,
-        _app: &mut Application<GlobalAppData>,
+        app: &mut Application<GlobalAppData>,
         scene: &mut GameScene,
         world: &mut World<GlobalAppData, GameScene, Message>,
         _input: &[InputEvent],
     ) -> Result<(), String> {
-        if scene.state.game.time_of_last_tick.elapsed().unwrap().as_millis() >= scene.state.game.tick_length as u128 {
+        let mut tick = scene.state.game.tick_length;
+        if app.window.is_key_pressed(Key::Space) {
+            tick /= 3;
+        }
+
+        if scene.state.game.time_of_last_tick.elapsed().unwrap().as_millis() >= tick as u128 {
             world.messages.send_to_3::<HeadSystem, BodySystem, FoodSystem>(Message::GameTick)?;
             scene.state.game.time_of_last_tick = SystemTime::now();
         }
